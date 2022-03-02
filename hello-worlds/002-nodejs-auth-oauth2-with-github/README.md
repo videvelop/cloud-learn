9184dd6daa9a90691a54fbc64ea8f0ab475f893a
videvelop <vimala@everapptech.com>
2022-03-02 12:25:07 +0530

    readme updates

--- hello-worlds/002-nodejs-auth-oauth2-with-github/README.md
+++ hello-worlds/002-nodejs-auth-oauth2-with-github/README.md
@@ -1,12 +1,31 @@
+This is an implementation of OAuth2.0 Authorization Code flow.
 
-Start the nodejs server for backchannel authentication.  Before starting, remember to fill up sample.env with your credentials and rename the file as .env.
+Prereq:
+1. Go to github.com and create OAuth Apps by accessing Settings -> Developer settings -> Oauth Apps.  https://github.com/settings/developers
+
+Note the Client ID and Client secrets.
+
+Auth Control Flow:
+1. when client is invoked using http://localhost:3000 , user is shown with sign-in link.  When the user clicks, below github URL is invoked with client_id and redirect_uri.  https://github.com/login/oauth/authorize?client_id=a5b3c833c88183daf88d&redirect_uri=http://localhost:9000/oauth/successcallback
+2. github.com, on receiving this request, since client_id is valid, it'll show the UI to sign-in to the user.  If user gives valid credentials, then access_code is sent to redirect_uri.
+3. the server side, on receiving the get request for http://localhost:9000/oauth/successcallback , sends client_secret + client_id + access_code to "https://github.com/login/oauth/access_token".  github.com will return back with access token.
+4. server side invokes client with the access_token.
+http://localhost:3000/?access_token=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
+5. If access_token is present in the client side, it is understood as the user is authenticated provided this access_token is validated by github.
+
+Serer:
+1. Copy sample.env as .env.
+2. Fillup the below values
+GITHUB_OAUTH_CLIENT_ID=
+GITHUB_OAUTH_CLIENT_SECRET=
+3. Start the nodejs server for backchannel authentication.  
 ```
 cd serverapp
 npm i
 npm run start
 ```
 
-
+Proxy for CORS:
 The below avoids cors issues
 ```
 npm install -g local-cors-proxy
--- hello-worlds/002-nodejs-auth-oauth2-with-github/clientapp/src/App.js
+++ hello-worlds/002-nodejs-auth-oauth2-with-github/clientapp/src/App.js
@@ -7,7 +7,14 @@ const GITHUB_OAUTH_CLIENT_ID = "a5b3c833c88183daf88d";
 const GITHUB_OAUTH_SUCCESS_CALLBACK = "/oauth/successcallback";
 const GITHUB_URL_FOR_LOGIN = `https://github.com/login/oauth/authorize?client_id=${GITHUB_OAUTH_CLIENT_ID}&redirect_uri=http://localhost:9000${GITHUB_OAUTH_SUCCESS_CALLBACK}`;
 
-const GITHUB_LOCAL_PROXY_URL = "http://localhost:8010/proxy/user"
+//a url called as http://localhost:8010/proxy/user will go as https://api/github.com/user,
+//provided a proxy is running in localhost as below
+//lcp --proxyUrl https://api.github.com/
+//lcp is an npm module.  It gives the proxy url http://localhost:8010/proxy/user for https://api.github.com/
+//this proxy url helps to avoid cors issue when you test all client & server in localhost.
+//request header with "origin"="localhost" cannot request resource from another domain api.github.com
+
+//const GITHUB_LOCAL_PROXY_URL = "http://localhost:8010/proxy/user"
 const GITHUB_NOPROXY_URL = "https://api.github.com/";
 
 function App() {
@@ -15,13 +22,16 @@ function App() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
+    //window.location.search provides query string..
+    //here, query string is expected to have access_token with valid value
     const token = new URLSearchParams(window.location.search).get(
       "access_token"
     );
 
+    console.log(window.location.search);
     //get user details from github using access token token received through oauth
     axios
-      .get(GITHUB_LOCAL_PROXY_URL, {
+      .get(GITHUB_NOPROXY_URL, {
         headers: {
           Authorization: "token " + token,
         },
@@ -38,6 +48,7 @@ function App() {
   }, []);
 
   console.log("useEffect done");
+
 
   return (
     <div className="App text-center container-fluid">
